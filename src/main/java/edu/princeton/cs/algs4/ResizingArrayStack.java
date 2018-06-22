7b9faa7cea0d3defd07009470b51d8e631204309
陈宏伟 <chenhongwei1112@163.com>
2018-06-22 08:52:17 +0800

    1.重命名.

--- src/main/java/edu/princeton/cs/algs4/ResizingArrayStack.java
+++ src/main/java/edu/princeton/cs/algs4/ResizingArrayStack.java
@@ -2,18 +2,18 @@ package edu.princeton.cs.algs4;
 
 import java.util.Iterator;
 
-public class FixedCapacityStackOfStrings<Item> implements Iterable<Item>{
+public class ResizingArrayStack<Item> implements Iterable<Item>{
     private Item [] a;
     private int N;
 
-    public FixedCapacityStackOfStrings(int cap){
+    public ResizingArrayStack(int cap){
         a = (Item[]) new Object[cap];
         N = 0;
     }
 
     public void push(Item item){
         if (isFull()) {
-            reSize(2*a.length);
+            resize(2*a.length);
         }
         a[N++] = item; 
     }
@@ -22,7 +22,7 @@ public class FixedCapacityStackOfStrings<Item> implements Iterable<Item>{
         Item item = a[--N];
         a[N] = null;
         if (N > 0 && N == a.length / 4) {
-            reSize(a.length/2);
+            resize(a.length/2);
         }
         return item;
     }
@@ -35,7 +35,7 @@ public class FixedCapacityStackOfStrings<Item> implements Iterable<Item>{
         return N == a.length;
     }
 
-    public void reSize(int max){
+    public void resize(int max){
         Item[] new_arr = (Item[]) new Object[max];
         for (int i = 0; i < N; i++) {
             new_arr[i] = a[i];
@@ -48,10 +48,10 @@ public class FixedCapacityStackOfStrings<Item> implements Iterable<Item>{
     }
 
     public Iterator<Item> iterator(){
-        return new FixedCapacityStackOfStringsIterator();
+        return new ReverseArrayIterator();
     }
 
-    private class FixedCapacityStackOfStringsIterator implements Iterator<Item> {
+    private class ReverseArrayIterator implements Iterator<Item> {
         private int i = N;
         public boolean hasNext() {
             return i > 0;
@@ -65,8 +65,8 @@ public class FixedCapacityStackOfStrings<Item> implements Iterable<Item>{
     }
 
     public static void main(String[] args) {
-        FixedCapacityStackOfStrings<String> s;
-        s = new FixedCapacityStackOfStrings<String>(2);
+        ResizingArrayStack<String> s;
+        s = new ResizingArrayStack<String>(2);
         while (!StdIn.isEmpty()) {
             String new_item = StdIn.readString();
             if (!new_item.equals("-")) {
